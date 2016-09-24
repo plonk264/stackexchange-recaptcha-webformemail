c084fc17cb035d4bb445e78fc48b93a6e553fe43
plonk420 <plonk420@users.noreply.github.com>
2016-09-24 05:22:49 -0600

    Create mail.php
    source by Tony, fixed by sailesh from http://stackoverflow.com/questions/28273491/recaptcha-and-php-to-send-an-email-form

---
+++ mail.php
@@ -0,0 +1,37 @@
+// Checking For correct reCAPTCHA
+$response = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=SECRETKEY&response=" . $captcha . "&remoteip=" . $_SERVER['REMOTE_ADDR']);
+if ($response . success == false) {
+    echo "Your CAPTCHA response was wrong.";
+    exit ;
+} else {
+    // Checking For Blank Fields..
+    if ($_POST["vname"] == "" || $_POST["vemail"] == "" || $_POST["msg"] == "") {
+        echo "Fill All Fields..";
+    } else {
+        // Check if the "Sender's Email" input field is filled out
+        $email = $_POST['vemail'];
+        // Sanitize E-mail Address
+        $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+        // Validate E-mail Address
+        $email = filter_var($email, FILTER_VALIDATE_EMAIL);
+        if (!$email) {
+            echo "Invalid Sender's Email";
+        } else {
+            $to = 'johdoe@example.com';
+            $subject = 'Test';
+            $message = $_POST['msg'];
+            $headers = 'From:' . $email . "\r\n";
+            // Sender's Email
+            // Message lines should not exceed 70 characters (PHP rule), so wrap it
+            $message = wordwrap($message, 70, "\r\n");
+            // Send Mail By PHP Mail Function
+            if (mail($to, $subject, $message, $headers)) {
+                echo "Your mail has been sent successfully!";
+            } else {
+                echo "Failed to send email, try again.";
+                exit ;
+            }
+        }
+    }
+}
+?>
